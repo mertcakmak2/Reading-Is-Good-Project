e4108c7da3153e53a1bffc11a2df1ef90f1e158c
mertcakmak <mertcakmak2@gmail.com>
2022-11-23 23:52:46 +0300

    AuthController added.

---
+++ src/main/java/com/project/readingisgood/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.project.readingisgood.controller;
+
+
+import com.project.readingisgood.model.request.LoginRequestModel;
+import com.project.readingisgood.result.DataResult;
+import com.project.readingisgood.result.SuccessDataResult;
+import com.project.readingisgood.service.auth.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(path = "")
+    public ResponseEntity<DataResult> login(@RequestBody @Valid LoginRequestModel loginRequestModel) {
+        var dataResult = new SuccessDataResult<String>("Signed in.", authService.login(loginRequestModel));
+        return new ResponseEntity<DataResult>(dataResult, HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/project/readingisgood/unit_tests/auth/AuthControllerUnitTests.java
@@ -0,0 +1,38 @@
+package com.project.readingisgood.unit_tests.auth;
+
+import com.project.readingisgood.controller.AuthController;
+import com.project.readingisgood.model.request.LoginRequestModel;
+import com.project.readingisgood.result.DataResult;
+import com.project.readingisgood.result.SuccessDataResult;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerUnitTests {
+
+    @MockBean
+    AuthController authController;
+
+    @Test
+    void login() {
+        LoginRequestModel loginRequestModel = new LoginRequestModel(
+                "mertcakmak2@gmail.com",
+                "password"
+        );
+        var dataResult = new SuccessDataResult<String>("Signed in.", "jwt-token");
+        var expectedResponse = new ResponseEntity<DataResult>(dataResult, HttpStatus.OK);
+
+        Mockito.when(authController.login(loginRequestModel)).thenReturn(expectedResponse);
+        var response = authController.login(loginRequestModel);
+
+        assertEquals(expectedResponse.getStatusCode(), response.getStatusCode());
+    }
+}
