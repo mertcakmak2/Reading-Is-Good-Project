a48ccf162cca1dc97b2d8785ccaa6faafd83d487
mertcakmak <mertcakmak2@gmail.com>
2022-11-23 22:13:10 +0300

    JwtUtil added

---
+++ src/main/java/com/project/readingisgood/jwt/JwtUtil.java
@@ -0,0 +1,77 @@
+package com.project.readingisgood.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+
+    Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+
+    private String secret = "reading_is_good_secret_key";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        Claims claims = null;
+        try {
+            claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return claims;
+    }
+
+    public Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+    public String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(getOneDayDate())
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    public Boolean validateToken(String token, String userName) {
+        final String username = extractUsername(token);
+        return (username.equals(userName) && !isTokenExpired(token));
+    }
+
+    public Date getOneDayDate() {
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.MINUTE, 1440);
+        return c.getTime();
+    }
+}
+
---
+++ src/test/java/com/project/readingisgood/unit_tests/JwtUtilTests.java
@@ -0,0 +1,125 @@
+package com.project.readingisgood.unit_tests;
+
+import com.project.readingisgood.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JwtUtilTests {
+
+    @MockBean
+    JwtUtil jwtUtil;
+
+    private String secret = "jwt_key";
+
+    @Test
+    void extractUsername_test_method_should_extract_username(){
+        String expectedUserName = "mertcakmak2@gmail.com";
+        String token = getSignedJwt(expectedUserName);
+        Mockito.when(jwtUtil.extractUsername(token)).thenReturn(expectedUserName);
+        String userName = jwtUtil.extractUsername(token);
+
+        assertEquals(expectedUserName, userName);
+    }
+
+    @Test
+    void extractExpiration_test_method_should_extract_expiration(){
+        String expectedUserName = "mertcakmak2@gmail.com";
+        String token = getSignedJwt(expectedUserName);
+        Date expectedDate = new Date();
+        Mockito.when(jwtUtil.extractExpiration(token)).thenReturn(expectedDate);
+        Date date = jwtUtil.extractExpiration(token);
+
+        assertNotNull(date);
+    }
+
+    @Test
+    void extractAllClaims_test_method_should_extract_all_claims(){
+        String expectedUserName = "mertcakmak2@gmail.com";
+        String token = getSignedJwt(expectedUserName);
+        var expectedClaims = extractAllClaims(token);
+        Mockito.when(jwtUtil.extractAllClaims(token)).thenReturn(expectedClaims);
+        var claims = jwtUtil.extractAllClaims(token);
+
+        assertEquals(expectedUserName,claims.get("username"));
+    }
+
+    @Test
+    void isTokenExpired_test_method_should_check_token_expire(){
+        String expectedUserName = "mertcakmak2@gmail.com";
+        String token = getSignedJwt(expectedUserName);
+        var expectedTokenExpiredState = true;
+        Mockito.when(jwtUtil.isTokenExpired(token)).thenReturn(expectedTokenExpiredState);
+        var isTokenExpired = jwtUtil.isTokenExpired(token);
+
+        assertEquals(expectedTokenExpiredState, isTokenExpired);
+    }
+
+    @Test
+    void generateToken_test_method_should_generate_token(){
+        String userName = "mertcakmak2@gmail.com";
+        String expectedJwt = getSignedJwt(userName);
+        Mockito.when(jwtUtil.generateToken(userName)).thenReturn(expectedJwt);
+        var jwt = jwtUtil.generateToken(userName);
+
+        assertEquals(expectedJwt, jwt);
+    }
+
+    @Test
+    void createToken_test_method_should_create_token(){
+        String userName = "mertcakmak2@gmail.com";
+        String expectedJwt = getSignedJwt(userName);
+        var expectedClaims = extractAllClaims(expectedJwt);
+        Mockito.when(jwtUtil.createToken(expectedClaims,userName)).thenReturn(expectedJwt);
+        var jwt = jwtUtil.createToken(expectedClaims,userName);
+
+        assertEquals(expectedJwt, jwt);
+    }
+
+    @Test
+    void getOneDayDate_test_method_should_get_date(){
+        Mockito.when(jwtUtil.getOneDayDate()).thenReturn(new Date());
+        var oneDayDate = jwtUtil.getOneDayDate();
+
+        assertNotNull(oneDayDate);
+    }
+
+    @Test
+    void validateToken_test_method_should_validate_token(){
+        String userName = "mertcakmak2@gmail.com";
+        String jwt = getSignedJwt(userName);
+        Mockito.when(jwtUtil.validateToken(jwt,userName)).thenReturn(true);
+        var isValidate = jwtUtil.validateToken(jwt,userName);
+
+        assertTrue(isValidate);
+    }
+
+    String getSignedJwt(String subject){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username",subject);
+
+        return Jwts.builder().setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 5 * 60 * 60 * 1000))
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    Claims extractAllClaims(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return claims;
+    }
+}
